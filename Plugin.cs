ce7321c51d6bbae26abc3d356b0260eb3c3e3a8e
Soggy_Pancake <54160598+Soggy-Pancake@users.noreply.github.com>
2021-10-13 14:45:24 -0700

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,133 @@
+﻿using BS_Utils.Utilities;
+using BS_Utils;
+using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.ViewControllers;
+using BeatSaberMarkupLanguage.GameplaySetup;
+using BeatSaberMarkupLanguage.Attributes;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct MouseInput
+{
+    public int dx;
+    public int dy;
+    public uint mouseData;
+    public uint dwFlags;
+    public uint time;
+    public IntPtr dwExtraInfo;
+}
+
+[Flags]
+public enum InputType
+{
+    Mouse = 0,
+    Keyboard = 1,
+    Hardware = 2
+}
+
+[Flags]
+public enum MouseEventF
+{
+    Absolute = 0x8000,
+    HWheel = 0x01000,
+    Move = 0x0001,
+    MoveNoCoalesce = 0x2000,
+    LeftDown = 0x02,
+    LeftUp = 0x04,
+    RightDown = 0x0008,
+    RightUp = 0x0010,
+    MiddleDown = 0x0020,
+    MiddleUp = 0x0040,
+    VirtualDesk = 0x4000,
+    Wheel = 0x0800,
+    XDown = 0x0080,
+    XUp = 0x0100
+}
+
+
+namespace CookieSaber
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("User32.dll")]
+        public static extern bool SetCursorPos(int x, int y);
+        [DllImport("User32.dll")]
+        private static extern IntPtr GetMessageExtraInfo();
+        [DllImport("User32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        public static bool ModEnabled;
+        public static int ClickMethod = 0;
+        public static int MouseX = 0;
+        public static int MouseY = 0;
+
+        [Init]
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            BSEvents.noteWasCut += NoteCut; //Subscribe to notecut event
+            BS_Utils.Utilities.Config ConfigVariable = new BS_Utils.Utilities.Config("CookieSaber");
+            ModEnabled = ConfigVariable.GetBool("CookieSaber", "Enabled", true, true);
+            ClickMethod = ConfigVariable.GetInt("CookieSaber", "Method", 1, true);
+            MouseX = ConfigVariable.GetInt("CookieSaber", "MouseX", 20, true);
+            MouseY = ConfigVariable.GetInt("CookieSaber", "MouseY", 10, true);
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Log.Info("Ready to click some cookies!");
+            GameplaySetup.instance.AddTab("CookieSaber", "CookieSaber.UI.SettingsTab.bsml", CookieSaberUI.CookieSaberUI.instance);
+        }
+
+        public void NoteCut(NoteData notedata, NoteCutInfo cutinfo, int multiplier)
+        {
+            Log.Info($"ModEnabled value: {ModEnabled}");
+            Log.Info($"Click Method value: {ClickMethod}");
+            if (ModEnabled && cutinfo.allIsOK)
+            {
+                if (ClickMethod == 1)
+                {
+                    SetCursorPos(MouseX, MouseY);
+                }
+                mouse_event(0x02, 0, 0, 0, 0); //Mouse Button Down
+                mouse_event(0x04, 0, 0, 0, 0); //Mouse Button Up
+            }
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            BS_Utils.Utilities.Config ConfigVariable = new BS_Utils.Utilities.Config("CookieSaber");
+            ConfigVariable.SetBool("CookieSaber", "Enabled",  ModEnabled);
+            ConfigVariable.SetInt("CookieSaber", "Method", ClickMethod);
+            ConfigVariable.SetInt("CookieSaber", "MouseX", MouseX);
+            ConfigVariable.SetInt("CookieSaber", "MouseY", MouseY);
+        }
+    }
+}
---
+++ UI/ModifierViewController.cs
@@ -0,0 +1,62 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using System.Collections.Generic;
+using System;
+
+namespace CookieSaberUI
+{
+    class CookieSaberUI : NotifiableSingleton<CookieSaberUI>
+    {
+        //NotifiableSingleton allows bind-value to be used
+        //PersistentSingleton doesn't
+
+        //massive thanks to Exomanz for helping with the view controller
+        [UIValue("Enabled")]
+        private bool Enabled
+        {
+            get => CookieSaber.Plugin.ModEnabled;
+            set => CookieSaber.Plugin.ModEnabled = value;
+        }
+
+        [UIValue("clickMethods")] //has to be list<object> for some reason
+        List<object> clickMethods = new List<object>() { "Current Position", "Absolute Position" };
+
+        [UIValue("clickMethod")]
+        private string clickMethod
+        {
+            get => (string)clickMethods[CookieSaber.Plugin.ClickMethod];
+            set => CookieSaber.Plugin.ClickMethod = clickMethods.IndexOf(value); //Current pos = 0, Absolute pos = 1, Rel pos = 2 (not doing relative for now)
+        }
+
+        [UIValue("MouseX")]
+        private int MouseX
+        {
+            get => CookieSaber.Plugin.MouseX;
+            set { 
+                CookieSaber.Plugin.MouseX = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        [UIValue("MouseY")]
+        private int MouseY
+        {
+            get => CookieSaber.Plugin.MouseY;
+            set { 
+                CookieSaber.Plugin.MouseY = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        [UIAction("getCursor")]
+        void getCursor() 
+        { 
+            CookieSaber.Plugin.GetCursorPos(out CookieSaber.Plugin.POINT mouse);
+            MouseX = mouse.X;
+            CookieSaber.Plugin.MouseX = mouse.X;
+            MouseY = mouse.Y;
+            CookieSaber.Plugin.MouseY = mouse.Y;
+            NotifyPropertyChanged();
+        }
+    }
+}
